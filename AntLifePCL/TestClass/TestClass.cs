99ae2e0439c9eefd2c471b31519bc6ae7c785dba
adiZ33 <adix2562@gmail.com>
2018-02-05 22:52:21 +0100

    Pola #1

--- AntLifePCL/TestClass/TestClass.cs
+++ AntLifePCL/TestClass/TestClass.cs
@@ -10,5 +10,23 @@ namespace AntLifePCL
     {
         public int TestClassId { get; set; }
         public string TestClassName { get; set; }
+
+        #region Bezpieczne pole
+
+        private int testInt;
+
+        public int TestInt
+        {
+            get { return testInt; }
+            set
+            {
+                if (testInt < 10)
+                    testInt = 10;
+                else
+                testInt = value;
+            }
+        }
+
+        #endregion
     }
 }
