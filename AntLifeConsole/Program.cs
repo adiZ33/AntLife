c15aea4859b7e730e4ee4b6a22e19fcef93eccb1
adiZ33 <adix2562@gmail.com>
2018-02-05 22:44:30 +0100

    Koncept

---
+++ AntLifeConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using AntLifePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AntLifeConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Owady
+            //LatająceOwady: Owady
+            //Osa: LatajaceOwady
+            //ZiemneOwady: Owady
+        }
+    }
+}
